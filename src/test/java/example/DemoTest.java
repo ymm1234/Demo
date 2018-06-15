7847d976adedf32a32819ba240862dca18202f00
yanmiaomiao <yanmiaomiao@hzzl.net>
2018-06-15 16:24:08 +0800

    修改人员: yanmiaomiao 修改时间: 2018-06-15 16:22 修改描述：练习题:从输入文件读取字符串，使用多线程进行排序，然后写入输出文件

---
+++ src/test/java/example/DemoTest.java
@@ -0,0 +1,16 @@
+package example;
+
+import org.junit.Test;
+
+public class DemoTest {
+	@Test
+	  public void testQuickSort() {
+	    Demo one = new Demo();
+	    try {
+	      one.quickSort();
+	    }catch (Exception e)
+	    {
+
+	    }
+	  }
+}
